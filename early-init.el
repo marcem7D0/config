9b6a902a1ae504ae430b6c8c4a4ee8a46874ae56
marcem <76575210+marcem7D0@users.noreply.github.com>
2025-11-12 16:40:58 +0100

    Update

--- early-init.el
+++ early-init.el
@@ -1,14 +1,19 @@
 ;; Set GC for performance
 (setq gc-cons-threshold (* 1024 1024 200)) ;; 200 MB
 
+(setq custom-safe-themes t)
+
 ;; Add Melpa
 (require 'package)
 
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
 
-;; Set font
-(set-frame-font "Iosevka NF 11")
+(when (eq system-type 'darwin)
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier nil))
+
+(setq font-lock-support-mode 'jit-lock-mode)
 
 ;; Set custom-file location
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
