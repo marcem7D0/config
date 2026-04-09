85c090fcca3e3c8ea30b844b73cbcf13b822004e
marcem7D0 <marcellmittnacht@gmail.com>
2026-04-09 22:28:22 +0200

    update config 1775766502

--- init.el
+++ init.el
@@ -14,3 +14,33 @@
   :ensure t
   :config
   (load-theme 'nordic-night t))
+
+(use-package vterm
+  :ensure t)
+
+(use-package treesit-auto
+  :ensure t
+  :config
+  (global-treesit-auto-mode))
+
+(use-package rust-mode
+  :ensure t
+  :hook(rust-mode . (lambda() (setq indent-tabs-mode nil))))
+
+(keymap-global-set "C-x 2" 'my-split-window-and-switch)
+(keymap-global-set "C-x p" 'my-other-window-backwards)
+(keymap-global-set "C-c t" 'my-open-vterm-split-window)
+
+(defun my-other-window-backwards ()
+  (interactive)
+  (other-window -1))
+
+(defun my-split-window-and-switch ()
+  (interactive)
+  (split-window-right)
+  (other-window 1))
+
+(defun my-open-vterm-split-window ()
+  (interactive)
+  (my-split-window-and-switch)
+  (vterm))
